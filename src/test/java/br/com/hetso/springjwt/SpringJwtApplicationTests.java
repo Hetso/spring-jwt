97c04e2f639a548eaa8fe5143669fdcf80b292ee
Guilherme dos Santos <gdossantos970@gmail.com>
2022-04-21 18:04:04 -0300

    First commit

---
+++ src/test/java/br/com/hetso/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hetso.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
